bd1007579d7a416dde5d9762834b71e6c1bcac79
Judy Odiakose <ojudy007@gmail.com>
2026-04-30 10:28:53 +0100

    Included resend for auto email notification

---
+++ utils/email.js
@@ -0,0 +1,16 @@
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+async function sendEmail({ to, subject, html }) {
+  return await resend.emails.send({
+    from: process.env.EMAIL_FROM,
+    to,
+    subject,
+    html,
+  });
+}
+
+module.exports = {
+  sendEmail,
+};
